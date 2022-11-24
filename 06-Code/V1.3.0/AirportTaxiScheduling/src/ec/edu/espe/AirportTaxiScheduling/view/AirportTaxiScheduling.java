d1bae50532a700ab695d9044ceafa1924537dc47
Justin <jjvillarroel1@espe.edu.ec>
2022-11-24 00:10:01 -0500

    Code V1.3.0

---
+++ 06-Code/V1.3.0/AirportTaxiScheduling/src/ec/edu/espe/AirportTaxiScheduling/view/AirportTaxiScheduling.java
@@ -0,0 +1,262 @@
+package ec.edu.espe.AirportTaxiScheduling.view;
+
+import ec.edu.espe.AirportTaxiScheduling.model.Traveler;
+import com.google.gson.Gson;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ProgressTeam,DCCO-ESPE
+ */
+public class AirportTaxiScheduling {
+    
+    public static void saveData(ArrayList<Traveler> travelers) {
+        Gson gson = new Gson();
+        String json = gson.toJson(travelers);
+        File file = new File("travelerList.json");
+        try ( FileWriter fw = new FileWriter(file);) {
+            fw.write(json);
+            System.out.println("\n");
+        } catch (Exception e) {
+            System.out.println("A problem occurred while saving the data ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Traveler> travelers = new ArrayList<Traveler>();
+        travelers = storeFileJson();
+        
+        Traveler traveler = new Traveler();
+        Scanner input = new Scanner(System.in);
+        int position[] = new int[1];
+        boolean exit = false;
+        int option;
+        position[0] = 0;
+
+        //readJsonFile(travelers,position);
+        //System.out.println(position[0]); 
+        while (!exit) {
+            System.out.println("ProgressTeam");
+            System.out.println("Menu");
+            System.out.println("Options:");
+            System.out.println("1.Enter traveler");
+            System.out.println("2.Search traveler");
+            System.out.println("3.Print all travelers");
+            System.out.println("4.Exit");
+
+            try {
+                System.out.println("Digit an option: ");
+                option = input.nextInt();
+
+                switch (option) {
+                    case 1:
+                        
+                        travelers = enterTraveler(travelers, traveler, position);
+                        saveData(travelers);
+                        
+                        position[0]++;
+                        break;
+                    case 2:
+                        long phoneNumber;
+                        int found = 0;
+                        if (position[0] != 0) {
+                            System.out.println("What is the phone number of the traveler?");
+                            phoneNumber = input.nextLong();
+                            for (int i = 0; i < travelers.size(); i++) {
+                                if (travelers.get(i).getPhoneNumber() == phoneNumber) {
+                                    System.out.println("===Traveler Data===");
+                                    printTraveler(travelers.get(i));
+                                    found++;
+                                }
+                            }
+                            if (found == 0) {
+                                System.out.println("Unregistered traveler");
+                            }
+                        } else {
+                            System.out.println("No travelers registered yet");
+                        }
+                        break;
+                    case 3:
+                        if (position[0] != 0) {
+                            for (int i = 0; i < travelers.size(); i++) {
+                                printTraveler(travelers.get(i));
+                            }
+                        } else {
+                            System.out.println("No travelers registered yet");
+                        }
+                        break;
+                    case 4:
+                        System.out.println("You exit was success");
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Nonexistent option");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect dataType");
+                System.out.println("If you don't see the menu again");
+                System.out.println("Write any character and press enter");
+                input.next();
+            }
+        }
+
+    }
+
+    private static void printTraveler(Traveler traveler) {
+        System.out.println(traveler);
+    }
+
+    public static ArrayList<Traveler> enterTraveler(ArrayList<Traveler> travelers, Traveler traveler, int position[]) {
+        String name;
+        String adress;
+        long phoneNumber;
+        String email;
+        String birthDate;
+        boolean repeatTraveler;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the name");
+        name = input.nextLine();
+
+        System.out.println("Write the adress");
+        adress = input.nextLine();
+
+        //do {
+            repeatTraveler = false;
+            System.out.println("Enter the phone number");
+            phoneNumber = input.nextLong();
+            input.nextLine();
+            //for (int i = 0; i < travelers.size(); i++) {
+                //if (travelers.get(i).getPhoneNumber() == phoneNumber) {
+                    //System.out.println("This Traveler was already registered");
+                    //System.out.println("Use other");
+                    //repeatTraveler = true;
+                //}
+            //}
+        //} while (repeatTraveler == true);
+
+        System.out.println("Write the email");
+        email = input.nextLine();
+
+        System.out.println("Enter the birth date");
+        birthDate = input.nextLine();
+
+        traveler = new Traveler(name, adress, phoneNumber, email, birthDate);
+        travelers.add(position[0], traveler);
+        
+        return travelers;
+    }
+
+    private static void createFile() {
+        File chickenList = new File("travelerList.json");
+        try {
+            PrintWriter writer = new PrintWriter(chickenList);
+            writer.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    private static void saveTraveler(Traveler traveler) {
+        File travelerList = new File("travelerList.csv");
+        String name = traveler.getName();
+        String adress = traveler.getAdress();
+        long phoneNumber = traveler.getPhoneNumber();
+        String email = traveler.getEmail();
+        String birthDate = traveler.getBirthDate();
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(travelerList, true));
+            writer.println("");
+            writer.print(name + ";");
+            writer.print(adress + ";");
+            writer.print(phoneNumber + ";");
+            writer.print(email + ";");
+            writer.print(birthDate + ";");
+            writer.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static ArrayList<Traveler> storeFileJson() {
+        Gson gson = new Gson();
+
+        ArrayList<Traveler> travelers = new ArrayList<>();
+        String jsonFile = "";
+        String[] jsonTraveler;
+
+        try ( Scanner scFile = new Scanner(new File("travelerList.json"))) {
+            while (scFile.hasNextLine()) {
+                jsonFile += scFile.nextLine();
+            }
+
+            jsonFile = jsonFile.replace("[", "");
+            jsonFile = jsonFile.replace("]", "");
+
+            jsonTraveler = jsonFile.split("},");
+
+            for (int i = 0; i < jsonTraveler.length; i++) {
+                if (i < jsonTraveler.length - 1) {
+                    travelers.add(gson.fromJson(jsonTraveler[i] + "}", Traveler.class));
+                } else {
+                    travelers.add(gson.fromJson(jsonTraveler[i], Traveler.class));
+                }
+            }
+            System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+            System.out.println("Data successfully uploaded");
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Error: File not open or found");
+        }
+
+        return travelers;
+    }
+
+    /*private static void saveTravelersToJson(ArrayList<Traveler> travelers) {
+       File chickenList = new File("chickenList.json");
+        Gson gson = new Gson();
+        String json = gson.toJson(chickens);
+        //String json = new Gson().toJson(chickens);
+        
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(chickenList, false)); 
+            //PrintWriter writer = new PrintWriter(chickenList); 
+            writer.print(json);   
+            writer.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);   
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }*/
+
+ /*private static void readJsonFile(ArrayList<Chicken> chickens, int position[]) {
+        String json = ""; 
+        Gson gson = new Gson();
+        Chicken chicken = new Chicken();
+        try {
+           BufferedReader reader = new BufferedReader(new FileReader("chickenlist.json"));
+           String line = "";
+        while ((line = reader.readLine())!= null){
+            json = line;
+            //chickens = gson.fromJson(json);
+            //chickens.add(position[0], chicken);
+            //position[0]++;
+        }
+            reader.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ChickenFarmSimulator.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ChickenFarmSimulator.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+    }*/
+}
