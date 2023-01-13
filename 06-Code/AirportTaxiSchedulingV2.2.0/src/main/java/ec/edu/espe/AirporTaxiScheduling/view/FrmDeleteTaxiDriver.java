a14f1e42d40630456e5eb7135053611c5438b62b
Jefferson Manuel Ulco Velasco <jeffersonulcoct@gmail.com>
2023-01-13 08:51:02 -0500

    AeropuertoTaxiProgramaciónV2.2.2

--- 06-Code/AirportTaxiSchedulingV2.2.0/src/main/java/ec/edu/espe/AirporTaxiScheduling/view/FrmDeleteTaxiDriver.java
+++ 06-Code/AirportTaxiSchedulingV2.2.0/src/main/java/ec/edu/espe/AirporTaxiScheduling/view/FrmDeleteTaxiDriver.java
@@ -4,14 +4,19 @@
  */
 package ec.edu.espe.AirporTaxiScheduling.view;
 
+import ec.edu.espe.AirporTaxiScheduling.controller.MongoData;
+import ec.edu.espe.AirporTaxiScheduling.model.TaxiDriver;
+import ec.edu.espe.AirporTaxiScheduling.view.AirportTaxiScheduling;
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author Leonardo Yaranga,Progress Team, DCCO-ESPE
+ * @author Jefferson Ulco,ProgressTeam,DCCO-ESPE
  */
 public class FrmDeleteTaxiDriver extends javax.swing.JFrame {
 
     /**
-     * Creates new form DeleteDriver
+     * Creates new form FrmDeleteTaxiDriver
      */
     public FrmDeleteTaxiDriver() {
         initComponents();
@@ -29,10 +34,14 @@ public class FrmDeleteTaxiDriver extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         btnBACK = new javax.swing.JButton();
+        txtCIdelete = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        btnDelete = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("DELETE TAXIDRIVER");
+        jLabel1.setFont(new java.awt.Font("Arial Black", 1, 18)); // NOI18N
+        jLabel1.setText("DELETE TAXI DRIVER");
 
         btnBACK.setText("<BACK");
         btnBACK.addActionListener(new java.awt.event.ActionListener() {
@@ -41,25 +50,64 @@ public class FrmDeleteTaxiDriver extends javax.swing.JFrame {
             }
         });
 
+        txtCIdelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtCIdeleteActionPerformed(evt);
+            }
+        });
+        txtCIdelete.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtCIdeleteKeyTyped(evt);
+            }
+        });
+
+        jLabel2.setText("Enter the C.I to delete:");
+
+        btnDelete.setText("ACCEPT");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(btnBACK)
-                .addGap(87, 87, 87)
-                .addComponent(jLabel1)
-                .addContainerGap(121, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(139, 139, 139)
+                        .addComponent(jLabel2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(19, 19, 19)
+                        .addComponent(btnBACK)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(123, 123, 123)
+                        .addComponent(txtCIdelete, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(75, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(btnDelete)
+                .addGap(157, 157, 157))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(11, 11, 11)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btnBACK)
-                    .addComponent(jLabel1))
-                .addContainerGap(261, Short.MAX_VALUE))
+                .addGap(25, 25, 25)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnBACK, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel1)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(txtCIdelete, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnDelete)
+                .addContainerGap(33, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -70,19 +118,52 @@ public class FrmDeleteTaxiDriver extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnBACKActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBACKActionPerformed
-        // TODO add your handling code here:
-           AirportTaxiScheduling airportTaxiScheduling;
-        airportTaxiScheduling = new AirportTaxiScheduling();
-        this.setVisible(false);
-        airportTaxiScheduling.setVisible(true);
+         AirportTaxiScheduling airportTaxiSheduling = new AirportTaxiScheduling();
+        airportTaxiSheduling.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_btnBACKActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        // TODO add your handling code here:
+        int option = JOptionPane.showConfirmDialog(this, "Do you want to continue?");
+        if (option == 0) {
+        TaxiDriver driver = new TaxiDriver( txtCIdelete.getText(),"", "", "", "", "", "", "", "", "");
+            MongoData.deleteDriver(driver);
+            JOptionPane.showMessageDialog(this, "Driver succesfully removed");
+            txtCIdelete.setText("");
+        } else if (option == 1) {
+            JOptionPane.showMessageDialog(this, "Operation Cancelled");
+        } else if (option == 2) {
+            AirportTaxiScheduling backBtm = new AirportTaxiScheduling();
+            backBtm.setVisible(true);
+            this.dispose();
+        }   
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
+    private void txtCIdeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtCIdeleteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtCIdeleteActionPerformed
+
+    private void txtCIdeleteKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtCIdeleteKeyTyped
+        // TODO add your handling code here:
+        int validNumber = evt.getKeyChar();
+        boolean number = validNumber >= 48 && validNumber <= 57;
+        if(!number){
+            evt.consume();
+                        JOptionPane.showMessageDialog(this, number + " only numbers", "Warning on input data", JOptionPane.WARNING_MESSAGE);
+
+        }
+        if(txtCIdelete.getText().length() >= 10){
+            evt.consume();
+        }
+    }//GEN-LAST:event_txtCIdeleteKeyTyped
 
     /**
      * @param args the command line arguments
@@ -110,9 +191,6 @@ public class FrmDeleteTaxiDriver extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(FrmDeleteTaxiDriver.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -124,7 +202,10 @@ public class FrmDeleteTaxiDriver extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBACK;
+    private javax.swing.JButton btnDelete;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtCIdelete;
     // End of variables declaration//GEN-END:variables
 }
