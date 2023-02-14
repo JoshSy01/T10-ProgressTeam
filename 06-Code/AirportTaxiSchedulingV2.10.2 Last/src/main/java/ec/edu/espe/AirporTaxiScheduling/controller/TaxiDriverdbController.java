32a95861cb1930cfd925c480423e16b1cb9828cb
Justin <jjvillarroel1@espe.edu.ec>
2023-02-13 21:22:03 -0500

    Validation of the Id, and clean code

--- 06-Code/AirportTaxiSchedulingV2.10.2 Last/src/main/java/ec/edu/espe/AirporTaxiScheduling/controller/TaxiDriverdbController.java
+++ 06-Code/AirportTaxiSchedulingV2.10.2 Last/src/main/java/ec/edu/espe/AirporTaxiScheduling/controller/TaxiDriverdbController.java
@@ -22,124 +22,144 @@ import org.bson.types.ObjectId;
  */
 public class TaxiDriverdbController {
 
-    public static void insertDriver(TaxiDriver driver) {
-        String uri = "mongodb+srv://lyaranga:tortilla@espe2210-oopsw7996.77wv341.mongodb.net/?retryWrites=true&w=majority";
-        try ( MongoClient mongoClient = MongoClients.create(uri)) {
+          public static void insertDriver(TaxiDriver driver) {
+                    String uri = "mongodb+srv://lyaranga:tortilla@espe2210-oopsw7996.77wv341.mongodb.net/?retryWrites=true&w=majority";
+                    try ( MongoClient mongoClient = MongoClients.create(uri)) {
 
-            MongoDatabase database = mongoClient.getDatabase("AirportTaxiScheduling");
-            try {
-                System.out.println("Connected successfully to server(TaxiDriver).");
+                              MongoDatabase database = mongoClient.getDatabase("AirportTaxiScheduling");
+                              try {
+                                        System.out.println("Connected successfully to server(TaxiDriver).");
 
-                MongoCollection<Document> collectionDriver = database.getCollection("TaxiDriver");
+                                        MongoCollection<Document> collectionDriver = database.getCollection("TaxiDriver");
 
-                Document drivers = new Document("_id", new ObjectId())
-                        .append("CI", driver.getCI())
-                        .append("name", driver.getName())
-                        .append("phoneNumber", driver.getPhoneNumber())
-                        .append("age", driver.getAge())
-                        .append("adress", driver.getAdress())
-                        .append("email", driver.getMail())
-                        .append("gender", driver.getGender())
-                        .append("vehicleMark", driver.getVehicleMark())
-                        .append("vehiclePlate", driver.getVehiclePlate())
-                        .append("vehicleYear", driver.getVehicleYear());
+                                        Document drivers = new Document("_id", new ObjectId())
+                                                .append("CI", driver.getCI())
+                                                .append("name", driver.getName())
+                                                .append("phoneNumber", driver.getPhoneNumber())
+                                                .append("age", driver.getAge())
+                                                .append("adress", driver.getAdress())
+                                                .append("email", driver.getMail())
+                                                .append("gender", driver.getGender())
+                                                .append("vehicleMark", driver.getVehicleMark())
+                                                .append("vehiclePlate", driver.getVehiclePlate())
+                                                .append("vehicleYear", driver.getVehicleYear());
 
-                collectionDriver.insertOne(drivers);
+                                        collectionDriver.insertOne(drivers);
 
-            } catch (MongoException me) {
-                System.out.println("An error occurred while attempting to connect: " + me);
-            }
+                              } catch (MongoException me) {
+                                        System.out.println("An error occurred while attempting to connect: " + me);
+                              }
 
-        }
-    }
+                    }
+          }
 
-    public static void deleteDriver(TaxiDriver driver) {
+          public static void deleteDriver(TaxiDriver driver) {
 
-        String uri = "mongodb+srv://lyaranga:tortilla@espe2210-oopsw7996.77wv341.mongodb.net/?retryWrites=true&w=majority";
+                    String uri = "mongodb+srv://lyaranga:tortilla@espe2210-oopsw7996.77wv341.mongodb.net/?retryWrites=true&w=majority";
 
-        try ( MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("AirportTaxiScheduling");
-            try {
-                MongoCollection collectionDriver = database.getCollection("TaxiDriver");
+                    try ( MongoClient mongoClient = MongoClients.create(uri)) {
+                              MongoDatabase database = mongoClient.getDatabase("AirportTaxiScheduling");
+                              try {
+                                        MongoCollection collectionDriver = database.getCollection("TaxiDriver");
 
-                Bson filter = Filters.eq("CI", driver.getCI());
-                collectionDriver.deleteOne(filter);
+                                        Bson filter = Filters.eq("CI", driver.getCI());
+                                        collectionDriver.deleteOne(filter);
 
-            } catch (MongoException me) {
-                System.out.println("An error occurred while attempting to connect: " + me);
-            }
+                              } catch (MongoException me) {
+                                        System.out.println("An error occurred while attempting to connect: " + me);
+                              }
 
-        }
-    }
+                    }
+          }
 
-    public static void updateDriver(TaxiDriver driver) {
-        String uri = "mongodb+srv://lyaranga:tortilla@espe2210-oopsw7996.77wv341.mongodb.net/?retryWrites=true&w=majority";
+          public static void updateDriver(TaxiDriver driver) {
+                    String uri = "mongodb+srv://lyaranga:tortilla@espe2210-oopsw7996.77wv341.mongodb.net/?retryWrites=true&w=majority";
 
-        try ( MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("AirportTaxiScheduling");
+                    try ( MongoClient mongoClient = MongoClients.create(uri)) {
+                              MongoDatabase database = mongoClient.getDatabase("AirportTaxiScheduling");
 
-            try {
+                              try {
 
-                MongoCollection collectionDriver = database.getCollection("TaxiDriver");
+                                        MongoCollection collectionDriver = database.getCollection("TaxiDriver");
 
-                Bson filter = Filters.eq("CI", driver.getCI());
+                                        Bson filter = Filters.eq("CI", driver.getCI());
 
-                Bson updates = Updates.combine(Updates.set("CI", driver.getCI()),
-                        
-                        Updates.set("name", driver.getName()),
-                Updates.set("phoneNumber", driver.getPhoneNumber()),
-                Updates.set("age", driver.getAge()),
-                Updates.set("adress", driver.getAdress()),
-                Updates.set("email", driver.getMail()),
-                Updates.set("gender", driver.getGender()),
-                Updates.set("vehicleMark", driver.getVehicleMark()),
-                Updates.set("vehiclePlate", driver.getVehiclePlate()),
-                Updates.set("vehicleYear", driver.getVehicleYear()));
+                                        Bson updates = Updates.combine(Updates.set("CI", driver.getCI()),
+                                                Updates.set("name", driver.getName()),
+                                                Updates.set("phoneNumber", driver.getPhoneNumber()),
+                                                Updates.set("age", driver.getAge()),
+                                                Updates.set("adress", driver.getAdress()),
+                                                Updates.set("email", driver.getMail()),
+                                                Updates.set("gender", driver.getGender()),
+                                                Updates.set("vehicleMark", driver.getVehicleMark()),
+                                                Updates.set("vehiclePlate", driver.getVehiclePlate()),
+                                                Updates.set("vehicleYear", driver.getVehicleYear()));
 
-                collectionDriver.updateOne(filter, updates);
+                                        collectionDriver.updateOne(filter, updates);
 
-            } catch (MongoException me) {
-                System.out.println("An error occurred while attempting to connect: " + me);
-            }
+                              } catch (MongoException me) {
+                                        System.out.println("An error occurred while attempting to connect: " + me);
+                              }
 
-        }
-    }
-    public static TaxiDriver findDriver(TaxiDriver driver) {
-        String Data;
-        Gson gson = new Gson();
-        String uri = "mongodb+srv://lyaranga:tortilla@espe2210-oopsw7996.77wv341.mongodb.net/?retryWrites=true&w=majority";
+                    }
+          }
 
-        try ( MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("AirportTaxiScheduling");
-            try {
-                MongoCollection<Document> collectionDriver = database.getCollection("TaxiDriver");
+          public static TaxiDriver findDriver(TaxiDriver driver) {
+                    String Data;
+                    Gson gson = new Gson();
+                    String uri = "mongodb+srv://lyaranga:tortilla@espe2210-oopsw7996.77wv341.mongodb.net/?retryWrites=true&w=majority";
 
-                Bson filter = Filters.eq("CI", driver.getCI());
-                try {
-                    Document doc = collectionDriver.find(Filters.and(filter)).first();
-                    Data = doc.toJson();
-                    TypeToken<TaxiDriver> type = new TypeToken<TaxiDriver>() {
-                    };
-                    driver = gson.fromJson(Data, type.getType());
+                    try ( MongoClient mongoClient = MongoClients.create(uri)) {
+                              MongoDatabase database = mongoClient.getDatabase("AirportTaxiScheduling");
+                              try {
+                                        MongoCollection<Document> collectionDriver = database.getCollection("TaxiDriver");
 
-                } catch (Exception e) {
-                    System.out.println("Data not found");
-                }
+                                        Bson filter = Filters.eq("CI", driver.getCI());
+                                        try {
+                                                  Document doc = collectionDriver.find(Filters.and(filter)).first();
+                                                  Data = doc.toJson();
+                                                  TypeToken<TaxiDriver> type = new TypeToken<TaxiDriver>() {
+                                                  };
+                                                  driver = gson.fromJson(Data, type.getType());
 
-            } catch (MongoException me) {
-                System.out.println("An error occurred while attempting to connect: " + me);
-            }
-        }
+                                        } catch (Exception e) {
+                                                  System.out.println("Data not found");
+                                        }
 
-        return driver;
-    }
-    
-    public static ArrayList<TaxiDriver> loadFromDatabase(ArrayList<TaxiDriver> objects, MongoDatabase database, String collectionName) {
-        Gson gson = new Gson();
-        MongoCollection<Document> objectsCollection = database.getCollection(collectionName);
-        FindIterable<Document> result = objectsCollection.find(Filters.gt("phoneNumber", 0));
-        result.forEach(d -> objects.add(gson.fromJson(d.toJson(), TaxiDriver.class)));
-        return objects;
-    }
-    
+                              } catch (MongoException me) {
+                                        System.out.println("An error occurred while attempting to connect: " + me);
+                              }
+                    }
+
+                    return driver;
+          }
+
+          public static ArrayList<TaxiDriver> loadFromDatabase(ArrayList<TaxiDriver> objects, MongoDatabase database, String collectionName) {
+                    Gson gson = new Gson();
+                    MongoCollection<Document> objectsCollection = database.getCollection(collectionName);
+                    FindIterable<Document> result = objectsCollection.find(Filters.gt("phoneNumber", 0));
+                    result.forEach(d -> objects.add(gson.fromJson(d.toJson(), TaxiDriver.class)));
+                    return objects;
+          }
+
+          public static boolean IdValidation(String IdValidator) {
+                    int sum = 0;
+                    for (int i = 0; i < IdValidator.length() - 1; i++) {
+                              int digit = Integer.parseInt(IdValidator.substring(i, i + 1));
+                              if (i % 2 == 0) {
+                                        digit *= 2;
+                                        if (digit > 9) {
+                                                  digit -= 9;
+                                        }
+                              }
+                              sum += digit;
+                    }
+
+                    int check = 10 - (sum % 10);
+                    if (check == 10) {
+                              check = 0;
+                    }
+
+                    return check == Integer.parseInt(IdValidator.substring(9, 10));
+          }
 }
