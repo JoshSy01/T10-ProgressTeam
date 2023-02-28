d625b0366a5ed9ac4e624bd60bbebe5997b60f01
Jefferson Manuel Ulco Velasco <jeffersonulcoct@gmail.com>
2023-02-27 19:05:15 -0500

    SingletonV1.0.1

---
+++ 06-Code/AirportTaxiSchedulingV3.1.0/src/main/java/ec/edu/espe/AirporTaxiScheduling/model/TarifSingleton.java
@@ -0,0 +1,34 @@
+package ec.edu.espe.AirporTaxiScheduling.model;
+
+/**
+ *
+ * @author Jefferson Ulco,ProgressTeam,DCCO-ESPE
+ */
+public class TarifSingleton {
+private static TarifSingleton instance;
+    private float tarifValue;
+    private TarifSingleton(){    
+        this.tarifValue = 0.40F;
+    }
+    
+    public void modifyTax(float tarifValue){
+        this.tarifValue = tarifValue;
+    }
+    
+    public static TarifSingleton getInstance() {
+        if(instance == null){ 
+            instance = new TarifSingleton();
+        }
+        return instance;
+    }
+    
+    public float totalTarif(float kilometro) {
+        float tarif = kilometro * this.tarifValue;
+        return tarif;
+    }
+
+    public float getTarifValue() {
+        return tarifValue;
+    }
+}
+
