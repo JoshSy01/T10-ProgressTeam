b497314fb859f178ac5eadfd1e4b7c21777f36d0
Jefferson Manuel Ulco Velasco <jeffersonulcoct@gmail.com>
2023-03-01 04:57:33 -0500

    commented code removeV1.0.2

---
+++ 06-Code/AirportTaxiSchedulingV3.1.0 last/src/test/java/ec/edu/espe/AirporTaxiScheduling/model/TarifSingletonTest.java
@@ -0,0 +1,48 @@
+package ec.edu.espe.AirporTaxiScheduling.model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Justin Villarroel, ProgressTeam, DCCO-ESPE
+ */
+public class TarifSingletonTest {
+
+    public TarifSingletonTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+          /**
+           * Test of totalTarif method, of class TarifSingleton.
+           */
+          @Test
+          public void testTotalTarif() {
+                    System.out.println("totalTarif");
+                    float kilometro = 0.0F;
+                    TarifSingleton instance = null;
+                    float expResult = 0.0F;
+                    float result = instance.totalTarif(kilometro);
+                    assertEquals(expResult, result, 0);
+          }
+
+
+}
